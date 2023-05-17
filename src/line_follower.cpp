803501c57f8129838e0037cb80d1d27342f2fc98
JustTheCoolest <balu.somenumbers@gmail.com>
2023-05-17 12:58:17 +0530

    Reorganised

--- src/line_follower.cpp
+++ src/line_follower.cpp
@@ -1,8 +1,8 @@
-#include<Arduino.h>
+#include <Arduino.h>
 
-// uno code 
-// bool check;
 float error;
+
+// Pin list
 int ir1 = 3;
 int ir2 = 4;
 int ir3 = 5;
@@ -11,47 +11,46 @@ int ir5 = 7;
 int ir6 = 8;
 int ir7 = 9;
 int ir8 = 10;
+int pins[8] = {ir1, ir2, ir3, ir4, ir5, ir6, ir7, ir8};
+
 int ir[8];
 
-int perr;
-int p, i=0, d;
-int kp=0, ki=0, kd=0;
-int pid;
 
-void geterror2();
-void geterror();
-void getpid(); 
 void setup() {
   // put your setup code here, to run once:
   
   Serial.begin(9600);
 }
 
-float getDeviation(int sensors[], int n = 8){
-  int counter = 0;
-  float deviation = 0;
+
+void sensorsRead(int *sensor_data = ir, int *pins = pins, int n = 8){
   for(int i=0; i<n; i++){
-    if(sensors[i]){
-      counter += 1;
-      deviation += i-3.5;
+    sensor_data[i] = digitalRead(pins[i]);
+  }
+}
+
+
+float getDeviation(int sensor_data[], int n = 8){ 
+  float index_shift = n/2+0.5;
+  int number_of_high_sensors = 0;
+  float sum_of_high_sensors = 0;
+  for(int i=0; i<n; i++){
+    if(sensor_data[i]){
+      number_of_high_sensors += 1;
+      
+      sum_of_high_sensors += i-(index_shift);
     }
   }
-  if(not counter){
+  if(!number_of_high_sensors){
     return 0;
   }
-  deviation = -deviation/counter;
+  float deviation = -sum_of_high_sensors/number_of_high_sensors;
   return deviation;
 }
 
+
 void loop() {
-  ir[0] = digitalRead(ir1);
-  ir[1] = digitalRead(ir2);
-  ir[2] = digitalRead(ir3);
-  ir[3] = digitalRead(ir4);
-  ir[4] = digitalRead(ir5);
-  ir[5] = digitalRead(ir6);
-  ir[6] = digitalRead(ir7);
-  ir[7] = digitalRead(ir8);
+  sensorsRead();
   for(int i=0;i<8;i++) {
   Serial.print(ir[i]);
   }
@@ -63,55 +62,12 @@ void loop() {
     // put your main code here, to run repeatedly:
 
 }
-void geterror() {
-  if (ir[0]== 1 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0 && ir[5] == 0 && ir[6] == 0 && ir[7] == 0) {
-    error=7;
-  }
-  else if (ir[0]== 1 && ir[1] == 1 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0 && ir[5] == 0 && ir[6] == 0 && ir[7] == 0) {
-    error=6;
-  }
-  else if (ir[0]== 0 && ir[1] == 1 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0 && ir[5] == 0 && ir[6] == 0 && ir[7] == 0) {
-    error=5;
-  }
-  else if (ir[0]== 0 && ir[1] == 1 && ir[2] == 1 && ir[3] == 0 && ir[4] == 0 && ir[5] == 0 && ir[6] == 0 && ir[7] == 0) {
-    error=4;
-  }
-  else if (ir[0]== 0 && ir[1] == 0 && ir[2] == 1 && ir[3] == 0 && ir[4] == 0 && ir[5] == 0 && ir[6] == 0 && ir[7] == 0) {
-    error=3;
-  }
-  else if (ir[0]== 0 && ir[1] == 0 && ir[2] == 1 && ir[3] == 1 && ir[4] == 0 && ir[5] == 0 && ir[6] == 0 && ir[7] == 0) {
-    error=2;
-  }else if (ir[0]== 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 1 && ir[4] == 0 && ir[5] == 0 && ir[6] == 0 && ir[7] == 0) {
-    error=1;
-  }
-  else if (ir[0]== 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 1 && ir[4] == 1 && ir[5] == 0 && ir[6] == 0 && ir[7] == 0) {
-    error=0;
-  }
-  else if (ir[0]== 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 1 && ir[5] == 0 && ir[6] == 0 && ir[7] == 0) {
-    error=-1;
-  }
-  else if (ir[0]== 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 1 && ir[5] == 1 && ir[6] == 0 && ir[7] == 0) {
-    error=-2;
-  }
-  else if (ir[0]== 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0 && ir[5] == 1 && ir[6] == 0 && ir[7] == 0) {
-    error=-3;
-  }
-  else if (ir[0]== 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0 && ir[5] == 1 && ir[6] == 1 && ir[7] == 0) {
-    error=-4;
-  }
-  else if (ir[0]== 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0 && ir[5] == 0 && ir[6] == 1 && ir[7] == 0) {
-    error=-5;
-  }
-  else if (ir[0]== 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0 && ir[5] == 0 && ir[6] == 1 && ir[7] == 1) {
-    error=-6;
-  }
-  else if (ir[0]== 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0 && ir[5] == 0 && ir[6] == 0 && ir[7] == 1) {
-    error=-7;
-  }
-  else if (ir[0]== 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0 && ir[5] == 0 && ir[6] == 0 && ir[7] == 0) {
-    error=9999;
-  }
-}
+
+
+int perr;
+int p, i=0, d;
+int kp=0, ki=0, kd=0;
+int pid;
 
 void getpid() {
   perr = error;
@@ -120,3 +76,4 @@ void getpid() {
   d = error-perr;
   pid = (kp*p)+(ki*i)+(kd*d);
 }
+
