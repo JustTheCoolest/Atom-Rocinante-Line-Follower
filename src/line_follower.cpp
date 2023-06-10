3a39c7b38429b0ea41bae55d22716323066cdb79
Logamithran M.R <89205253+loga115@users.noreply.github.com>
2023-06-10 09:02:28 +0530

    Update line_follower.cpp
    oh my god theres so many lines

--- src/line_follower.cpp
+++ src/line_follower.cpp
@@ -50,7 +50,30 @@ void copyArray(int const a[], int b[], int const n = 8){
     b[i] = a[i];
   }
 }
+void printArray(int array[], int n=8){
+  for(int i=0; i<n; ++i){
+    Serial.print(array[i]);
+    Serial.print(" ");
+  }
+  Serial.print("\n");
+}
+void digitaliseData(int sensor_data[], int const thresholds[], int const  n = 8){
+void copyArray(int const a[], int b[], int const n = 8){
+  for(int i=0; i<n; ++i){
+    sensor_data[i] = sensor_data[i] > thresholds[i];
+    b[i] = a[i];
+  }
+}
 
+
+int findEndOfStreak(int sensor_data[], bool *streak_present, int j, int const n = 8){
+  for(++j; j<n-2; ++j){
+    if(!sensor_data[j]){
+      if(!sensor_data[++j]){
+        *streak_present = true;
+        return j+1;
+        
+        
 void calibrate(int thresholds[], byte const pins[], int const n = 8){
   digitalWrite(lmotor,HIGH);
   digitalWrite(lmotorn,LOW);
@@ -77,7 +100,47 @@ void calibrate(int thresholds[], byte const pins[], int const n = 8){
   digitalWrite(rmotor, HIGH);
   digitalWrite(rmotorn, HIGH);
 }
+        
 
+int findEndOfStreak(int sensor_data[], bool *streak_present, int j, int const n = 8){
+  for(++j; j<n-2; ++j){
+    if(!sensor_data[j]){
+      if(!sensor_data[++j]){
+        *streak_present = true;
+        return j+1;
+      }
+      continue;
+    }
+  }
+  return -1;
+}
+        
+void filterSensors(int sensor_data[] = ir, int n = 8){
+  bool streak_present = false;
+  /*
+  if(!hasStreak(sensor_data)){
+    return; // // If there is no streak, a single 1 is not treated as noise, it could be our true data
+  }
+  */
+  for(int i=0; i<n; ++i){
+    if(!sensor_data[i]){
+      continue;
+    }
+    int j = findEndOfStreak(sensor_data, &streak_present, i);
+    if(j<-1){
+      return;
+    }
+    if(j==i+3){
+      sensor_data[i] = 0;
+    }
+    i = j;
+        thresholds[i] = 0.75*maxValues[i] + 0.25*minValues[i] + 0.5;
+  }
+  digitalWrite(lmotor, HIGH);
+  digitalWrite(lmotorn, HIGH);
+  digitalWrite(rmotor, HIGH);
+  digitalWrite(rmotorn, HIGH);
+}
   
   
   // Task : Stop at full black
@@ -101,8 +164,46 @@ signed  int value_assign[8]={-4,-3,-2,-1,1,2,3,4};
     float Position=sum_high/high_sensors;
     return Position;
   }
-    
+    if(!number_of_high_sensors){
+    return 0;  // edge case handling -- if everything is zero, return 0 instead of NaN
+  }
+  float deviation = -sum_of_high_sensors/number_of_high_sensors; // error value for pid
+  return deviation;
+}
 
+// Task : Stop at full black
+
+    
+bool isEqual(int const a[], int const b[], int const n = 8){
+  for(int i=0; i<n; ++i){
+    if(a[i]!=b[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+
+void testFilterSensors(){
+  int test_data[][2][8] = {
+    {{0, 0, 0, 0, 0, 0, 0, 0},{0, 0, 0, 0, 0, 0, 0, 0}}, // blank
+    {{0, 0, 0, 1, 0, 0, 1, 0},{0, 0, 0, 1, 0, 0, 1, 0}}, // single ones, undifferentiable between real data and noise
+    {{1, 1, 1, 0, 0, 0, 0, 0},{1, 1, 1, 0, 0, 0, 0, 0}}, // Simple streak
+    {{1, 1, 1, 0, 0, 1, 0, 0}, {1, 1, 1, 0, 0, 0, 0, 0}}, // Stray correction
+    {{1, 1, 1, 0, 0, 1, 0, 1}, {1, 1, 1, 0, 0, 1, 0, 1}}, // Dual streak with gap in between one. Single gaps are also considered part of the streak cause what if the gap...
+    // ...actuallly the error and not the 1s
+    {{1, 1, 1, 0, 1, 0, 0, 1}, {1, 1, 1, 0, 1, 0, 0, 0}}, // Streak with gap and stray
+    {{0, 1, 0, 0, 0, 1, 0, 1}, {0, 0, 0, 0, 0, 1, 0, 1}}  // Stray to the left of streak //flag
+  };
+  int const n=8;
+  for(int unsigned i=0; i<sizeof(test_data); ++i){
+    int *sensor_data = test_data[i][0];
+    int *expected = test_data[i][1];
+    filterSensors(sensor_data);
+    //assert(isEqual(sensor_data, expected, n));
+  }
+}
+signed  int value_assign[8]={-4,-3,-2,-1,1,2,3,4};
 
 float getPID(){
   //Error is the difference between the postion of the bot and the position we want it to be
@@ -121,6 +222,48 @@ float getPID(){
   return pid;
   
 }
+      
+      
+      
+ void startSpinning(){
+  digitalWrite(lmotor,HIGH);
+  digitalWrite(lmotorn,LOW);
+  digitalWrite(rmotor,LOW);
+  digitalWrite(rmotorn,HIGH);
+}
+
+void stopMoving(){
+  digitalWrite(lmotor, HIGH);
+  digitalWrite(lmotorn, HIGH);
+  digitalWrite(rmotor, HIGH);
+  digitalWrite(rmotorn, HIGH);
+  return pid;
+
+}
+
+int thresholds[8];      
+void calibrate(int thresholds[], byte const pins[], int const n = 8){
+  startSpinning();
+  int sensorData[n];
+  int minValues[n], maxValues[n];
+  for(int i=0; i<50; ++i){
+    sensorsRead(sensorData, pins, n);
+    for(int j=0; j<n; ++j){
+      if(sensorData[j]<minValues[j] || i==0){
+        minValues[j] = sensorData[j];
+      }
+      if(sensorData[j]>maxValues[j] || i==0){
+        maxValues[j] = sensorData[j];
+      }
+    }
+  }
+  for(int i=0; i<n; ++i){
+    thresholds[i] = 0.75*maxValues[i] - 0.25*minValues[i] + 0.5;
+  }
+  stopMoving();
+}
+
+
 void writeMotors(const int pid, const int sensorStates[]){
     for(int i=0; i<8; ++i){
       if(sensorStates[i]){
@@ -131,7 +274,7 @@ void writeMotors(const int pid, const int sensorStates[]){
     }
 }
 
-int thresholds[8];
+
 
 void printArray(int array[], int n=8){
   for(int i=0; i<n; ++i){
